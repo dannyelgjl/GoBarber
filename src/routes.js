2c4c523fda6cd1894ad9adb3b824362d95aabd89
daniel gomes <dannyelgjl@gmail.com>
2020-01-31 00:06:08 -0300

    UserController atualizado para criar novos users

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Caio Henrique',
-    email: 'Henquire@gmail.com.br',
-    password_hash: '123412312356',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
